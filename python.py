ff3bec4e2444afed8c57569ee4424833b992400b
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-06-29 13:20:11 +0900

    파이썬 문법 공부 완료

--- python.py
+++ python.py
@@ -341,35 +341,74 @@
 # game_over()
 
 # ===============================================================
-# 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
+# # 주어진 코드를 활용하여 부동산 프로그램을 작성하시오.
 
-# (출력 예제)
-# 총 3대의 매물이 있습니다.
-# 강남 아파트 매매 10억 2010년
-# 마포 오피스텔 전세 5억 2007년
-# 송파 빌라 월세 500/50 2000년
+# # (출력 예제)
+# # 총 3대의 매물이 있습니다.
+# # 강남 아파트 매매 10억 2010년
+# # 마포 오피스텔 전세 5억 2007년
+# # 송파 빌라 월세 500/50 2000년
 
-# [코드]
-class House:
-    # 매물 초기화
-    def __init__(self, location, house_type, deal_type, price, completion_year):
-        self.location = location
-        self.house_type = house_type
-        self.deal_type = deal_type
-        self.price = price
-        self.completion_year = completion_year
+# # [코드]
+# class House:
+#     # 매물 초기화
+#     def __init__(self, location, house_type, deal_type, price, completion_year):
+#         self.location = location
+#         self.house_type = house_type
+#         self.deal_type = deal_type
+#         self.price = price
+#         self.completion_year = completion_year
 
-    # 매물 정보 표시
-    def show_detail(self):
-        print("{0} {1} {2} {3} {4}년".format(self.location, self.house_type, self.deal_type,
-              self.price, self.completion_year))
+#     # 매물 정보 표시
+#     def show_detail(self):
+#         print("{0} {1} {2} {3} {4}년".format(self.location, self.house_type, self.deal_type,
+#               self.price, self.completion_year))
 
 
-houses = []
-houses.append(House("강남", "아파트", "매매", "10억", 2010))
-houses.append(House("마포", "오피스텔", "전세", "5억", 2007))
-houses.append(House("송파", "빌라", "월세", "500/50", 2000))
+# houses = []
+# houses.append(House("강남", "아파트", "매매", "10억", 2010))
+# houses.append(House("마포", "오피스텔", "전세", "5억", 2007))
+# houses.append(House("송파", "빌라", "월세", "500/50", 2000))
 
-print("총 {0}대의 매물이 있습니다.".format(len(houses)))
-for house in houses:
-    house.show_detail()
+# print("총 {0}대의 매물이 있습니다.".format(len(houses)))
+# for house in houses:
+#     house.show_detail()
+
+# ============================================================================
+# # 동네에 항상 대기 손님이 있는 맛있는 치킨집이 있스비다.
+# # 대기 손님의 치킨 요리 시간을 줄이고자 자동 주문 시스템을 제작하였습니다.
+# # 시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오
+
+# # 조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError 로 처리
+# #         출력 메세지 : "잘못된 값을 입력하였습니다."
+# # 조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
+# #         치킨 소진 시 사용자 정의 에러[SoldOutError]를 발생시키고 프로그램 종료
+# #         출력 메시지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
+
+# class SoldOutError(Exception):
+#     def __init__(self):
+#         pass
+
+
+# chicken = 10
+# waiting = 1
+# while(True):
+#     print("[남은 치킨 : {0}]".format(chicken))
+#     try:
+#         order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+#         if order < 1:
+#             raise ValueError
+#         elif order > chicken:
+#             print("재료가 부족합니다.")
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+#             waiting += 1
+#             chicken -= order
+
+#         if chicken == 0:
+#             raise SoldOutError
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except SoldOutError:
+#         print("제고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         break
