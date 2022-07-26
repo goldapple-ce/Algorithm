ff45dc6d2c1a1914e4c15bb28aca756dd0c225fd
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-07-26 21:12:02 +0900

    코테 20%

---
+++ cote/7-2.py
@@ -0,0 +1,21 @@
+# 이진 탐색
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start+end)//2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    elif array[mid] < target:
+        return binary_search(array, target, mid+1, end)
+
+
+n, target = map(int, input().split())
+array = list(map(int, input().split()))
+
+res = binary_search(array, target, 0, n-1)
+if res == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(res+1)
