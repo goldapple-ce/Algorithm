29335e49c1058030b7fdacb176e202589570a045
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-03-08 00:22:53 +0900

    [level 2] Title: 숫자의 표현, Time: 0.33 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        if(n < 3) return 1;
+        int answer = 0;
+        int left = 1,right = 1,sum = 0;
+        while (right <= (int)n/2 +1){
+            
+            if(sum+right <= n){
+                sum += right++;
+            }else{
+                sum -= left++;
+            }
+            
+            if(sum == n){
+                answer++;
+            }
+
+        }
+        
+        
+        return answer+1;
+    }
+}
