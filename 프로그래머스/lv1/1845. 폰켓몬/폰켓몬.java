4e5228ab2cdeb3fa467f51ae0010d39ea1984071
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-03-09 02:06:33 +0900

    [level 1] Title: 폰켓몬, Time: 3.11 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,8 @@
+import java.util.HashSet;
+class Solution {
+    public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int num: nums) set.add(num);
+        return nums.length/2 < set.size() ? nums.length/2 : set.size();
+    }
+}
