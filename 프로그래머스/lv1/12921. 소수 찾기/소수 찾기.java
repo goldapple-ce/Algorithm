90318ec61c081edc64daa7e5e74faa6c4c0481af
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-03-18 01:08:04 +0900

    [level 1] Title: 소수 찾기, Time: 23.73 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -4,15 +4,12 @@ class Solution {
         int answer = 0;
         boolean[] prime = new boolean[n+1];
         Arrays.fill(prime,true);
-        prime[0] = false;prime[1] = false;
-        for(int i = 2;i<=Math.sqrt(n)+1;i++){
+        for(int i = 2;i<n+1;i++){
             if(prime[i]){
+                answer++;
                 for(int j = (int)Math.pow(i,2);j<=n;j+=i) 
                     prime[j] = false;
             }
-        }
-        for(int i = 2;i<=n;i++){
-            if(prime[i]) answer++;
         }
         return answer;
     }
