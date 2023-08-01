6cc1801858c3575063f847f82a698a649ebf479d
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-01 17:20:57 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 148 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,50 +1,66 @@
+
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int maxSize = Integer.parseInt(br.readLine());
-        boolean[] switchs = new boolean[maxSize];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < maxSize; i++) {
-            switchs[i] = st.nextToken().equals("0") ? false : true;
-        }
-
-        int orderSize = Integer.parseInt(br.readLine());
-        for (int i = 0; i < orderSize; i++) {
-            st = new StringTokenizer(br.readLine());
-            int sex = Integer.parseInt(st.nextToken());
-            int num = Integer.parseInt(st.nextToken());
-
-            if (sex == 1) {
-                for (int j = num - 1; j < maxSize; j += num) {
-                    switchs[j] = !switchs[j];
-                }
-            } else {
-                switchs[num - 1] = !switchs[num - 1];
-                int left = num - 2, right = num;
-                while (0 <= left && right < maxSize && switchs[left] == switchs[right]) {
-                    switchs[left] = !switchs[left];
-                    switchs[right] = !switchs[right];
-                    left--;
-                    right++;
-                }
-            }
-        }
-        for (int i = 0; i < maxSize; i++) {
-            sb.append(switchs[i] ? "1 " : "0 ");
-            if ((i + 1) % 20 == 0)
-                sb.append("\n");
-        }
-
-        System.out.println(sb);
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] sw = new int[N+1];
+		sw[0]=-1;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i=1;i<N+1;i++) 
+			sw[i] = Integer.parseInt(st.nextToken());
+		
+		
+		int nums = Integer.parseInt(br.readLine());
+		
+		for(int k=0;k<nums;k++) {
+			st = new StringTokenizer(br.readLine());
+			
+			if(1==Integer.parseInt(st.nextToken())) {
+				// 남자면
+				int n = Integer.parseInt(st.nextToken());
+				
+				int i=n;
+				while(i<(N+1)) {
+					sw[i]= (sw[i]==0)? 1:0;
+					i+=n;
+				}
+				
+			}else {
+				//여자면
+				int n = Integer.parseInt(st.nextToken());
+				int s = n-1;
+				int e = n+1;
+				sw[n] = (sw[n]==0)? 1:0;
+				
+				while(0<=s&&s<N+1 && 0<=e&& e<N+1&&sw[s]==sw[e]) {
+					sw[s] = (sw[s]==0)? 1:0;
+					sw[e] = (sw[e]==0)? 1:0;
+					s--;
+					e++;
+				}
+			}
+			
+		}
+		
+		for(int i=1;i<N+1;i++) {
+			System.out.print(sw[i]+" ");
+			if(i%20==0)
+				System.out.println();
+		}
+		
+	}
 
 }
