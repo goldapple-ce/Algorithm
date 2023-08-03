c6048452bbae7cec9c9514fca7c7575a17013ccd
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-03 13:07:32 +0900

    [Gold V] Title: 신기한 소수, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -3,30 +3,36 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static String[] numType = {"1","2","3","5","7","9"};
+	static int[] numType = {1,2,3,5,7,9};
 	static int N;
+	static StringBuilder sb = new StringBuilder();
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+
 		N = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i < numType.length; i++) {
+		for(int i = 1; i < numType.length; i++) 
 			checkPrime(numType[i],1);
-		}
+		System.out.println(sb);
 	}
 
-	public static void checkPrime(String strNum, int depth) {
-		int num = Integer.parseInt(strNum);
-		for(int i = 2; i <= Math.sqrt(num); i++) {
-			if(num%i == 0) return;
-		}
+	public static void checkPrime(int num, int depth) {
+		if(!isPrime(num)) return;
 		if(depth == N) {
-			System.out.println(strNum);
+			sb.append(num).append("\n");
 			return;
 		}
 		
 		for(int i = 0; i < numType.length; i++) {
-			checkPrime(strNum+numType[i],depth+1);
+			checkPrime(num*10+numType[i],depth+1);
 		}
+	}
+	
+	public static boolean isPrime(int num) {
+		for(int i = 2; i <= Math.sqrt(num); i++) {
+			if(num%i == 0) return false;
+		}
+		return true;
 	}
 }
