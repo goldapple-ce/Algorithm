c6fd04a395c48683e94f9e72e0985e262655f14a
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-04 09:10:09 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 28948 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,16 +1,24 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-class Main {
-	public static void main(String[] args) throws NumberFormatException, IOException {
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		Queue<Integer> queue = new LinkedList<>();
-		for(int i = 1; i <= N; i++) queue.offer(i);
-		while(1 < queue.size()) {
-			queue.poll();
-			queue.offer(queue.poll());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		Deque<Integer> dq = new ArrayDeque<Integer>();
+		for(int i=1;i<=N;i++)
+			dq.addLast(i);
+		
+		while(dq.size()>1){
+			dq.pollFirst();
+			dq.addLast(dq.pollFirst());
 		}
-		System.out.println(queue.poll());
+		System.out.println(dq.getFirst());
 	}
 }
