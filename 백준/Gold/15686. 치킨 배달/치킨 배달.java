0502414e144a585e8f7af4aaf6f5ecfa238adc54
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-04 09:34:38 +0900

    [Gold V] Title: 치킨 배달, Time: 244 ms, Memory: 17548 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -1,27 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-class Position {
-	int row;
-	int col;
-
-	Position (int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-}
-
 public class Main {
 	static int N, M;
 	static int[][] map;
 	static ArrayList<Position> house = new ArrayList<>();
 	static ArrayList<Position> chicken = new ArrayList<>();
 	static int answer = Integer.MAX_VALUE;
-	static boolean[] open;
+	static boolean[] visited;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		N = Integer.parseInt(st.nextToken());
@@ -29,41 +18,36 @@ public class Main {
 
 		map = new int[N][N];
 
-		// 미리 집과 치킨집에 해당하는 좌표를 ArrayList에 넣어 둠.
-		for (int i = 0; i < N; i++) {
+		for (int row = 0; row < N; row++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+			for (int col = 0; col < N; col++) {
+				map[row][col] = Integer.parseInt(st.nextToken());
 
-				if (map[i][j] == 1) {
-					house.add(new Position(i, j));
-				} else if (map[i][j] == 2) {
-					chicken.add(new Position(i, j));
+				if (map[row][col] == 1) {
+					house.add(new Position(row, col));
+				} else if (map[row][col] == 2) {
+					chicken.add(new Position(row, col));
 				}
 			}
 		}
 
-		
-		open = new boolean[chicken.size()];
+		visited = new boolean[chicken.size()];
 
 		DFS(0, 0);
-		bw.write(answer + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(answer);
 	}
 
-	public static void DFS(int start, int cnt) {
-		if (cnt == M) {
+	public static void DFS(int now, int depth) {
+		if (depth == M) {
 			int res = 0;
 
-			for (int i = 0; i < house.size(); i++) {
+			for (int row = 0; row < house.size(); row++) {
 				int temp = Integer.MAX_VALUE;
 
-				for (int j = 0; j < chicken.size(); j++) {
-					if (open[j]) {
-						int distance = Math.abs(house.get(i).row - chicken.get(j).row)
-								+ Math.abs(house.get(i).col - chicken.get(j).col);
+				for (int col = 0; col < chicken.size(); col++) {
+					if (visited[col]) {
+						int distance = Math.abs(house.get(row).row - chicken.get(col).row)
+								+ Math.abs(house.get(row).col - chicken.get(col).col);
 						temp = Math.min(temp, distance);
 					}
 				}
@@ -74,10 +58,20 @@ public class Main {
 		}
 
 		// 백트래킹
-		for (int i = start; i < chicken.size(); i++) {
-			open[i] = true;
-			DFS(i + 1, cnt + 1);
-			open[i] = false;
+		for (int i = now; i < chicken.size(); i++) {
+			visited[i] = true;
+			DFS(i + 1, depth + 1);
+			visited[i] = false;
 		}
+	}
+}
+
+class Position {
+	int row;
+	int col;
+
+	public Position(int row, int col) {
+		this.row = row;
+		this.col = col;
 	}
 }
