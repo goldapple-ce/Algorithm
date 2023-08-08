961e358fee74444086fe12b4dc598c2228835d8a
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-08 16:48:53 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 2056 ms, Memory: 135580 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -1,73 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-import java.io.*;
-import java.util.*;
-
-class Main {
-	static int[][] map;
-	static int[][] dirType = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-	static int N, M;
-	static boolean[][] visited;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+public class Main {
+	static int[][] delta = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+	static int N;
+	static int M;
+	
+	public static boolean is_valid_coord(int r, int c) {
+		return 0<=r && r<N && 0<=c && c<M;
+	}
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		for (int row = 0; row < N; row++) {
+
+		int[][] adj = new int[N][M];
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int col = 0; col < M; col++)
-				map[row][col] = Integer.parseInt(st.nextToken());
-		}
-
-		for (int r = 0; r < R; r++) {
-			visited = new boolean[N][M];
-			for (int i = 0; i < Math.max(1, Math.min(N, M)-2); i++) {
-				rotate(i);
+			for (int j = 0; j < M; j++) {
+				adj[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		for(int row = 0; row < N; row++) {
-			for(int col = 0; col < M; col++)
-				sb.append(map[row][col]).append(' ');
-			sb.append('\n');
-		}
-		System.out.println(sb);
-	}
-
-	public static boolean inRange(int row, int col) {
-		return 0 <= row && row < N && 0 <= col && col < M;
-	}
-
-	public static void rotate(int n) {
-		Position now = new Position(n, n, map[n][n]);
-		for (int j = 0; j < 2*(N-2*n)+2*(M-2*n)-4; j++) {
-			for(int i = 0; i < 3; i++) {
-				int nDir = (now.dir + i)%4;
-				int nRow = now.row + dirType[nDir][0], nCol = now.col + dirType[nDir][1];
-				if(inRange(nRow,nCol) && !visited[nRow][nCol]) {
-					now.row = nRow; now.col = nCol;
-					now.dir = nDir;
-					break;
+		for(int j=0;j<R;j++) {
+			
+		
+		int r = 0, c = 0;
+		int d = 0;
+		boolean[][] visited= new boolean[N][M];
+			
+		for (int i = 0; i < (Math.min(N,M) / 2); i++) {
+			r = i;
+			c = i;
+			
+			int[]tmp = new int[2];
+			tmp[0]=adj[i][i];
+			
+			int cnt = 2 * ((N - 2 * i) + (M - 2 * i)) - 4;
+			
+			while(cnt>0){
+				
+				int nr = r + delta[d][0];
+				int nc = c + delta[d][1];
+				
+				if (is_valid_coord(nr, nc) && !visited[nr][nc]) {
+					visited[nr][nc] = true;
+					tmp[1] = adj[nr][nc];
+					adj[nr][nc]=tmp[0];
+					tmp[0] = tmp[1];
+					cnt--;		
+				} else {
+					nr = nr -delta[d][0];
+					nc = nc -delta[d][1];
+					d = (d + 1) % 4;
 				}
+				r = nr;
+				c = nc;
 			}
-			visited[now.row][now.col] = true;
-			int temp = map[now.row][now.col];
-			map[now.row][now.col] = now.val;
-			now.val = temp;
 		}
-	}
-}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for(int j=0;j<M;j++)
+				System.out.print(adj[i][j]+" ");
+			System.out.println();
+			
+		}
 
-class Position {
-	int row, col;
-	int dir, val;
-
-	public Position(int row, int col, int val) {
-		this.row = row;
-		this.col = col;
-		this.val = val;
-		this.dir = 0;
 	}
+
 }
