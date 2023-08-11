f2b55f17c70b617371e3e4674e35d322656d4b60
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-11 14:34:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 132 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,21 +1,39 @@
 
-import java.io.*;
-import java.util.*;
 
-class Main {
-	static Map<Integer,Integer> dict = new HashMap<>();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static List<Integer> answer;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		for(int three = 0; three <=5000/3; three++) {
-			for(int five = 0; five <= 1000;five++) {
-				if((three*3)+(five*5) == N) {
-					System.out.println(three+five);
-					return;
+		
+		
+		int answer = -1;
+		for(int i=0;i<=5000/3;i++) {
+			boolean isFinish = false;
+			
+			for(int j=0; j<=5000/5;j++) {
+				if(3*i + 5*j == N) {
+					answer = i+j;
+					isFinish=true;
+					break;
 				}
 			}
+			
+			if(isFinish) break;
 		}
-		System.out.println(-1);
+		
+		System.out.println(answer);
+		
 	}
+
 }
