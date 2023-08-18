716eebd8f247010d4fbd0ba0eef433db6bc6c4e2
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-18 11:48:29 +0900

    [Gold IV] Title: 알파벳, Time: 1976 ms, Memory: 301416 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,58 +1,52 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int R; // 세로
-    static int C;// 가로
-    static char [][] origin;
-    static Set <Character> data;
-    static int ans;
-    static int [] dx = {-1,0,1,0};
-    static int  [] dy = {0,-1,0,1};
-    public static void main(String[] args) throws Exception{
-        input();
-        ans =0;
-        data = new HashSet<>(26);
-        data.add(origin[0][0]);
-        dfs(0,0,1);
-        System.out.println(ans);
-    }
+	static int[][] dirType = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static int maxRow, maxCol;
+	static char[][] board;
+	static int answer = 0;
 
-    static void input() throws Exception{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        R = Integer.parseInt(st.nextToken());
-        C= Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		maxRow = Integer.parseInt(st.nextToken());
+		maxCol = Integer.parseInt(st.nextToken());
+		board = new char[maxRow][maxCol];
+		for (int row = 0; row < maxRow; row++) {
+			String strRow = br.readLine();
+			for (int col = 0; col < maxCol; col++) {
+				board[row][col] = strRow.charAt(col);
+			}
+		}
+		Set<Character> set = new HashSet<>();
+		set.add(board[0][0]);
+		dfs(0,0, set);
+		System.out.println(answer);
+	}
 
-        origin = new char[R][C];
+	public static void dfs(int row, int col, Set<Character> set) {
+		boolean flag = false;
+//		System.out.println("row : "+row+", col : "+col+", set :"+set.toString());
+		for (int dir = 0; dir < dirType.length; dir++) {
+			int nRow = row + dirType[dir][0];
+			int nCol = col + dirType[dir][1];
+			if (inRange(nRow, nCol) && !set.contains(board[nRow][nCol])) {
+//				Set<Character> nSet = new HashSet<>(set);
+				set.add(board[nRow][nCol]);
+				flag = true;
+				dfs(nRow, nCol, set);
+				set.remove(board[nRow][nCol]);
+			}
+		}
+		if (!flag)
+			answer = Math.max(answer, set.size());
+	}
 
-        for(int y=0; y<R; y++){
-            String ip = br.readLine();
-            for(int x=0; x<C;x++){
-                origin[y][x] = ip.charAt(x);
-            }
+	public static boolean inRange(int row, int col) {
+		return 0 <= row && row < maxRow && 0 <= col && col < maxCol;
+	}
 
-        }
-    }
-
-    static boolean in_range(int x,int y){
-        return 0<= x && x<C && 0<=y && y<R;
-    }
-    static void dfs(int x , int y, int depth){
-        ans = Math.max(ans,depth);
-        for(int dir=0;dir<4;dir++){
-            int cx = x+dx[dir];
-            int cy = y +dy[dir];
-            if(in_range(cx,cy) && !data.contains(origin[cy][cx])){
-                data.add(origin[cy][cx]);
-                dfs(cx,cy,depth+1);
-                data.remove(origin[cy][cx]);
-            }
-        }
-    }
 }
