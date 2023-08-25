eb2aa8d28d041a7663654e9f6019fa793461a1fc
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2023-08-25 14:40:14 +0900

    [Gold IV] Title: 최단경로, Time: 1148 ms, Memory: 110892 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,86 +1,106 @@
 
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int V,E;
-	static List<Vertex>[] graph;
-	static boolean[] visited;
-	static int[] dist;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
 	
+	static int V, E, start;
+	static List<List<Edge>> graph;
+	static int answer[];
+
+	static class Edge implements Comparable<Edge>{
+		int w;
+		int weight;
+		
+		public Edge(int w, int weight) {
+			super();
+			this.w = w;
+			this.weight = weight;
+		}
+
+
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.weight,o.weight);
+		}
+
+
+		@Override
+		public String toString() {
+			return "Edge [w=" + w + ", weight=" + weight + "]";
+		}
+		
+		
+	}
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		input();
+		dijkstra();
+		print();
+		
+	}
+
+	private static void print() {
+		for(int i=1;i<V+1;i++) {
+			System.out.println(answer[i]==Integer.MAX_VALUE? "INF": answer[i]);
+		}
+		
+	}
+
+	private static void input() throws Exception {
+		
+		st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
-		graph = new ArrayList[V+1];
-		visited = new boolean[V+1];
-		dist = new int[V+1];
-		for (int v = 0; v <= V; v++) {
-			dist[v] = Integer.MAX_VALUE;
-			graph[v] = new ArrayList<>();
+		
+		start  = Integer.parseInt(br.readLine());
+		
+		answer = new int[V+1];
+		graph = new ArrayList<>();
+		for(int i=0;i<V+1;i++) {
+			graph.add(new ArrayList<>());
+			answer[i]= Integer.MAX_VALUE;
 		}
-		int K = Integer.parseInt(br.readLine());
-
-		for (int e = 0; e < E; e++) {
+		
+		for(int i=0;i<E;i++) {
 			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-
-			graph[from].add(new Vertex(to, weight));
+			graph.get(s).add(new Edge(e,weight));
 		}
-		dijkstra(K);
+		
 	}
-	public static void dijkstra(int K) {
-		dist[K] = 0;
+	
+	private static void dijkstra() {
+		boolean visited[] = new boolean[V+1];
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.add(new Edge(start,0));
+		answer[start] = 0;
 		
-		PriorityQueue<Vertex> queue = new PriorityQueue<>();
-		queue.offer(new Vertex(K,0));
-		
-		while(!queue.isEmpty()) {
-			int now = queue.poll().idx;
+		while(!pq.isEmpty()) {
+			Edge now = pq.poll();
 			
-			if(visited[now]) continue;
-			visited[now] = true;
+			if(visited[now.w]) continue;
 			
-			for(Vertex vertex : graph[now]) {
-				if(dist[vertex.idx] > dist[now]+ vertex.weight) {
-					dist[vertex.idx]= dist[now]+ vertex.weight; 
-					queue.offer(new Vertex(vertex.idx,dist[vertex.idx]));
+			visited[now.w]=true;
+			
+			for(Edge e: graph.get(now.w)) {
+				if(visited[e.w]) continue;
+				if(answer[now.w]+e.weight < answer[e.w]) {
+					answer[e.w] = answer[now.w]+e.weight;
+					pq.add(new Edge(e.w,answer[e.w]));
 				}
 			}
 		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i < dist.length; i++) {
-			int d= dist[i];
-			if(d == Integer.MAX_VALUE) sb.append("INF");
-			else sb.append(d);
-			sb.append('\n');
-		}
-		System.out.println(sb);
-	}
-}
-
-class Vertex implements Comparable<Vertex> {
-	int idx, weight;
-
-	public Vertex(int to, int weight) {
-		this.idx = to;
-		this.weight = weight;
-	}
-
-	@Override
-	public String toString() {
-		return "Vertex [idx=" + idx + ", weight=" + weight + "]";
-	}
-
-
-
-	@Override
-	public int compareTo(Vertex o) {
-		return this.weight - o.weight;
 	}
 
 }
