e69cb48513faa021171ae436123fc58ef9778d13
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2024-11-24 19:54:28 +0900

    [Gold II] Title: 대표 선수, Time: 3760 ms, Memory: 110716 KB -BaekjoonHub

---
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+    static int N, M;
+    static PriorityQueue<Integer> [] students;
+    static int[] selected;
+    static int INF = 1_000_000_001;
+    static int answer = 1_000_000_001;
+
+    public static void main(String[] args) throws Exception {
+        input();
+        run();
+        print();
+    }
+    
+    static void run() throws Exception {
+        for(int n = 0; n < N; n++){
+            selected[n] = students[n].poll();
+        }
+
+        for(int s = 0; s < N * M; s++){
+            int maxIdx = 0, minIdx = 0;
+            for(int n = 1; n < N; n++){
+                if(selected[maxIdx] < selected[n]){
+                    maxIdx = n;
+                }else if(selected[minIdx] > selected[n]){
+                    minIdx = n;
+                }
+            }
+
+            answer = Math.min(answer, selected[maxIdx] - selected[minIdx]);
+            // System.out.println(Arrays.toString(selected));
+            // System.out.println(answer);
+            if(!students[minIdx].isEmpty()){
+                selected[minIdx] = students[minIdx].poll();
+            }
+        }
+        // System.out.println(Arrays.toString(students));
+    }
+    
+    static void input() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        students = new PriorityQueue[N];
+        selected = new int[N];
+
+        for(int n = 0; n < N; n++){
+            students[n] = new PriorityQueue<>();
+            st = new StringTokenizer(br.readLine());
+            for(int m = 0; m < M; m++){
+                students[n].offer(Integer.parseInt(st.nextToken()));
+            }
+        }
+    }
+    
+    static void print() throws Exception {
+        System.out.println(answer);
+    }
+}
