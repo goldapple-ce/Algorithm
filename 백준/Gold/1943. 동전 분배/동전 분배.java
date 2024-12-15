6d0ac1d6d740ff9c0220591c80ddb1096f7e5e84
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2024-12-15 18:44:06 +0900

    [Gold II] Title: 동전 분배, Time: 412 ms, Memory: 15400 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.java
+++ 백준/Gold/1943. 동전 분배/동전 분배.java
@@ -8,7 +8,7 @@ public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     static Money[] moneys;
-    static boolean[][] dp;
+    static boolean[] dp;
     static int N;
     static final int INF = 100_001;
     static int totalMoney;
@@ -26,27 +26,27 @@ public class Main {
             sb.append(0).append('\n');
             return;
         }
-        
-        dp[0][0] = true;
+
+        dp[0] = true;
         
         for(int n = 1; n <= N; n++){
             Money money = moneys[n-1];
-            for(int i = 0; i <= totalMoney/2; i++){
-                if(dp[n-1][i]){
+            for(int i = totalMoney / 2; i >= 0; i--){
+                if(dp[i]){
                     for(int c = 0; c <= money.cnt; c++){
-                        dp[n][i + money.value * c] = true;
+                        dp[i + money.value * c] = true;
                     }
                 }
             }
         }
-        sb.append(dp[N][totalMoney / 2] ? 1: 0).append('\n');
+        sb.append(dp[totalMoney / 2] ? 1: 0).append('\n');
     }
     
     static void input() throws Exception {
         N = Integer.parseInt(br.readLine());
 
         moneys = new Money[N];
-        dp = new boolean[N+1][INF];
+        dp = new boolean[INF];
         totalMoney = 0;
 
         for(int n = 0; n < N; n++){
