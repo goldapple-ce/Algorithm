11e51b881dac1b49b4499fb9fa8dfdeee843df75
goldapple-ce <84130518+goldapple-ce@users.noreply.github.com>
2025-04-08 18:22:35 +0900

    [Gold III] Title: 도로, Time: 228 ms, Memory: 23056 KB -BaekjoonHub

--- 백준/Gold/9344. 도로/도로.java
+++ 백준/Gold/9344. 도로/도로.java
@@ -22,35 +22,17 @@ public class Main{
     }
     
     static void run() throws Exception {
-        int beforeCost = kruskal();
-
-        if(parent[P] != 1 || parent[Q] != 1){
-            sb.append("YES");
-        }else{
-            roads.add(addRoad);
-            int afterCost = kruskal();
-    
-            sb.append(beforeCost > afterCost ?"YES" : "NO");
-        }
-
-        sb.append('\n');
-    }
-
-    static int kruskal(){
-        int cost = 0;
-
-        roads.sort(null);
-        for(int n = 0; n <= N;n++){
-            parent[n] = n;
-        }
         for(Road road : roads){
             if(find(road.from) != find(road.to)){
                 union(road.from, road.to);
-                cost += road.cost;
+                if((road.from == P && road.to == Q) || (road.from == Q && road.to == P)){
+                    sb.append("YES").append('\n');
+                    return;
+                }
             }
         }
 
-        return cost;
+        sb.append("NO").append('\n');
     }
 
     static int find(int x){
@@ -82,18 +64,20 @@ public class Main{
         roads = new ArrayList<>();
         parent = new int[N+1];
 
+        for(int n = 0; n <= N;n++){
+            parent[n] = n;
+        }
+
         for(int m = 0; m < M; m++){
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            if((from == P && to == Q) || (from == Q && to == P)){
-                addRoad = new Road(from, to, cost);
-            }else{
-                roads.add(new Road(from, to, cost));
-            }
+            roads.add(new Road(from, to, cost));
         }
+
+        roads.sort(null);
     }
     
     static void print() throws Exception {
